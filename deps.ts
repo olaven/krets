bdf4479f797a12dbb01eea8c2d39d4a9ff1f7ae2
olaven <olav@sundfoer.com>
2020-04-13 19:53:05 +0200

    updated dinatra version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { App, app, get, post, contentType } from 'https://denopkg.com/syumai/dinatra/mod.ts';
+export { App, app, get, post, contentType } from 'https://denopkg.com/syumai/dinatra@0.10.0/mod.ts';
 export { assertEquals, assert, assertNotEquals } from "https://deno.land/std/testing/asserts.ts";
--- handlers/response/response_handlers.ts
+++ handlers/response/response_handlers.ts
@@ -9,12 +9,12 @@ const get_responses = get("/api/brands/:brand_name/responses", ({params}) => {
     if (!responses && responses !== [])
         return [404, `Responses for ${brand_name} does not exist`];
 
-    return [200, contentType('json'), responses]; 
+    return [200, contentType('json'), JSON.stringify(responses)]; 
 });
 
 const post_response = post("/api/brands/:brand_name/responses", ({params}) => {
 
-
+    return "IMPLEMENT ME"
 });
 
 
