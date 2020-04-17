89eee0a278c0bf1652467dc81ad887674514f8dd
olaven <olav@sundfoer.com>
2020-04-17 11:31:13 +0200

    working on data values dissappearing

--- handlers/database.ts
+++ handlers/database.ts
@@ -1,11 +1,23 @@
 import { Brand, Response } from "./types.ts"
 
 const brands = new Map<string, Brand>()
-const responses = new Map<string, Response[]>(); 
+const brand_to_response = new Map<string, Response[]>(); 
+
 /**
  * Mock database before 
  * I get something "real"
  */
 export const database = {
-    brands, responses
+    brands: {
+        set: (name: string, brand: Brand) => 
+            brands.set(name, brand),
+        get: (name: string) => 
+            brands.get(name)
+    }, 
+    responses: {
+        set: (brand_name: string, responses: Response[]) => 
+            brand_to_response.set(brand_name, responses),
+        get: (brand_name: string) => 
+            brand_to_response.get(brand_name)
+    }
 }
