2778badb8727052c67e359edaa258484203e2858
olaven <olav@sundfoer.com>
2020-04-17 13:39:22 +0200

    index.html served with Dinatra

--- frontend/basic.jsx
+++
@@ -1,9 +0,0 @@
-import React from "https://dev.jspm.io/react";
-
-export const Basic = () => <div>
-
-    <h1>Easy way to get feedback!</h1>
-    
-    Register your page now: 
-    <input type="text"></input>
-</div>
