d7dd99497a99c08057e4744b84eb76e79daf0a28
olaven <olav@sundfoer.com>
2020-04-22 21:41:07 +0200

    JS working

--- public/client.js
+++ public/client.js
@@ -1,9 +1,6 @@
 import htm from "https://unpkg.com/htm@2.2.1/dist/htm.module.js";
-import {
-  h,
-  hydrate,
-} from "https://unpkg.com/preact@10.0.5/dist/preact.module.js";
-import { App } from "../source/app.js";
+import {h,hydrate,} from "https://unpkg.com/preact@10.0.5/dist/preact.module.js";
+import { App } from "./app.js";
 
 const html = htm.bind(h);
 
