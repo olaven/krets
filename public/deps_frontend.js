c1641369a55d5aab2e846ad746772b859af68af9
olaven <olav@sundfoer.com>
2020-04-29 21:42:28 +0200

    removed downloaded files that were not needed

--- public/deps_frontend.js
+++ public/deps_frontend.js
@@ -1,12 +1,6 @@
 import { h as preact } from 'https://cdn.pika.dev/preact@latest';
-import htm from "https://cdn.pika.dev/htm";
-//export { useState } from 'https://cdn.pika.dev/preact/hooks';
-
-/* import htm from "https://cdn.pika.dev/htm";
-import { _h as preact } from './preact.js'*/
+export { renderToString } from "https://cdn.pika.dev/preact-render-to-string";
 export { useState, useEffect } from './hooks.js' 
-
-
-export { renderToString } from "https://cdn.pika.dev/preact-render-to-string@5.1.6";
+import htm from "https://cdn.pika.dev/htm";
 
 export const  h = htm.bind(preact);
