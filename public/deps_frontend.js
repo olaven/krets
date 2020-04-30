db20e994c58af8520d67f8655a630dc7519573f4
olaven <olav@sundfoer.com>
2020-04-30 18:20:44 +0200

    made sure versions on server/client are equal and wrapped component in root-div

--- public/deps_frontend.js
+++ public/deps_frontend.js
@@ -1,4 +1,4 @@
-import { h as preact } from 'https://cdn.pika.dev/preact@latest';
+import { h as preact } from 'https://cdn.pika.dev/preact';
 export { renderToString } from "https://cdn.pika.dev/preact-render-to-string";
 export { useState, useEffect } from './hooks.js' 
 import htm from "https://cdn.pika.dev/htm";
--- source/handlers/frontend/rendering.js
+++ source/handlers/frontend/rendering.js
@@ -3,26 +3,26 @@ import { renderToString } from "../../../public/deps_frontend.js";
 
 export const renderBody = (Component, component_path, props) => {
 
-
     const stringified_props = JSON.stringify(props)
     const withProps = () => Component(stringified_props)
 
     const rendered = renderToString(withProps())
-    console.log("rendered", rendered);
 
     return `
         <html>
             <head>
                 <script type="module">
                 
-                    import htm from 'https://cdn.pika.dev/htm@2.2.1';
-                    import {h,hydrate,} from 'https://cdn.pika.dev/preact';
-                    const html = htm.bind(h);
+                    import { h as preact, hydrate } from 'https://cdn.pika.dev/preact';
+                    export { renderToString } from "https://cdn.pika.dev/preact-render-to-string";
+                    import htm from "https://cdn.pika.dev/htm";
+
+                    export const h = htm.bind(preact);
 
                     function client_render(Component) {
     
-                        const jsx = html \`<\${Component} page="All" />\`
-                        const hydrated = hydrate(jsx, document.body);
+                        const jsx = h \`<\${Component} page="All" />\`
+                        const hydrated = hydrate(jsx, document.getElementById("root"));
                         return hydrated
                     }
                                     
@@ -33,7 +33,9 @@ export const renderBody = (Component, component_path, props) => {
                 </script>
             </head>
             <body>
-                ${rendered}
+                    <div id="root">
+                        ${rendered}
+                    </div>
             </body>
         </html>`
 } 
