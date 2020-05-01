ed2eae97becbb34d9e62554db4c17d364fd5cdb3
olaven <olav@sundfoer.com>
2020-05-01 16:31:43 +0200

    refactored dotenv and added cear-function

--- dotenv.ts
+++ dotenv.ts
@@ -9,15 +9,27 @@ const get_pairs = (raw_text: string) => raw_text
     .split("\n")
     .map(line => line.split("="))
 
+const read_pair = async (path: string, action: (pair: [string, string]) => any) => {
+
+    const file_content = await read_file(path);
+    get_pairs(file_content)
+        .map(pair => pair as [string, string])
+        .forEach(action)
+}
+
 /**
  * Loads .env into Deno.env
  */
-export const dotenv = async () => {
-
-    const file_content = await read_file("./.env"); 
-    get_pairs(file_content).forEach(pair => {
-
-        const [key, value] = [pair[0], pair[1]];
+export const dotenv = async (path: string = `./.env`) => 
+    read_pair(path, ([key, value]) => {
         Deno.env.set(key, value);
-    }); 
-}
+    });
+
+/**
+ * Clears environment of 
+ * variables in .env
+ */
+export const clearenv = (path: string = `./.env`) => 
+    read_pair(path, ([key, _]) => {
+        Deno.env.set(key, "");
+    });
