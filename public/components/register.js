21658bbea511ab1afae0ad950c814613055ffd72
olaven <olav@sundfoer.com>
2020-05-05 18:11:14 +0200

    removed old attempts at registering, as simple link seems to have done the trick

--- public/components/register.js
+++ public/components/register.js
@@ -1,5 +1,5 @@
 import Layout from "./layout.js";
-import { h } from "../deps_frontend.js"
+import { h, useState, useEffect } from "../deps_frontend.js"
 import { get } from "../utils.js";
 
 const build_query = (config) => `?${Object.entries(config)
@@ -22,52 +22,17 @@ const build_query = (config) => `?${Object.entries(config)
 const Register = (props) => {
 
     const { auth0_client_id, auth0_domain, host_uri } = props; 
+    const [ url, setUrl ] = useState("")
 
-    const onImplicit = async () => {
+    useEffect(() => {
 
-        const query = build_query({
-            audience: `${auth0_domain}/api/v2`, 
-            scope: "profile email", 
-            response_type: "token", //required
-            client_id: auth0_client_id,//required
-            state: "test_state_krets",
-            nonce: "nonce_value",
-        })
-        const response = await get(`${auth0_domain}/authorize${query}`); 
-
-        console.log(response);
-
-    }
-    
-
-    console.log(props);
-    const onUniversal = async () => {
-
-        /**
-         GET https: //krets.eu.auth0.com/authorize?
-            response_type = code | token &
-            client_id = si4eE1tnSMdh0pwD4Rt6ZKPgLoqioxzG &
-            connection = CONNECTION &
-            redirect_uri = undefined &
-            state = STATE
-         */
-
-        const query = build_query({
-            response_type: "code",
-            client_id: auth0_client_id, 
-            //connection: "CONNECTION", 
-            redirect_uri: "https://localhost:8080/amloggedin", 
-            state: "KRETS_STATE"
-        }); 
-
-        const response = await get(`${auth0_domain}/authorize${query}`); 
-        console.log(response);
-    }
+        const url = `https://krets.eu.auth0.com/login?client=${auth0_client_id}&redirect_uri=${host_uri}`;
+        setUrl(url)
+    }, [auth0_client_id, auth0_domain, host_uri])
     
     return h `<${Layout}>
-        <button onClick=${onImplicit}>registrer implicit (not working)</button>
-        <button onClick=${onUniversal}>universal login</button>
-        <a href=${`https://krets.eu.auth0.com/login?client=${auth0_client_id}&redirect_uri=${host_uri}`}>Try link</a>
+        
+        <a href=${url}>Login</a>
     </${Layout}>`
 }
 
