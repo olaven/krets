07c123736739a5b246e9edc834ca380a534200e4
olaven <olav@sundfoer.com>
2020-05-08 16:32:22 +0200

    cleaded up home-component as AuthContext has filled its previos role

--- public/components/home.js
+++ public/components/home.js
@@ -1,41 +1,12 @@
 import Layout from "./layout.js";
-import { h, useContext } from "../deps_frontend.js"
-import { http, url } from "../utils/utils.js";
-import { AuthContext } from "../context/auth_context.js";
+import { h } from "../deps_frontend.js"
 
 
 
 const Home = (props) => {
 
-  const context = useContext(AuthContext);  
-
-  const fetch_user = async () => {
-
-      const endpoint = `https://krets.eu.auth0.com/userinfo`;
-      const { access_token } = url.parse_hash(endpoint);
-
-      const response = await http.get(endpoint, {
-          headers: {
-              "Authorization": `Bearer ${access_token}`
-          } 
-      }); 
-
-      console.log(response)
-      const text = await response.text();
-      console.log("text: ", text);
-  }
-
-  const retrieve_token = async () => {
-
-    //const url = "https://krets.eu.auth0.com/authorize"
-    const url = "/api/authorize";
-    const response = await http.get(url); 
-    console.log(response);
-  }
-
   return h`<${Layout} auth0=${props}>
-      <button onClick=${fetch_user}>fetch user</button>
-      <button onClick=${retrieve_token}>retrive token</button>
+      Krets home page
   </${Layout}>`
 }
 
