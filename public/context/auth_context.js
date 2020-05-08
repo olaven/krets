8bdfb39e51daad63ce1265930efd8e4ff561c103
olaven <olav@sundfoer.com>
2020-05-08 16:21:21 +0200

    moved user and token to effects

--- public/context/auth_context.js
+++ public/context/auth_context.js
@@ -3,6 +3,62 @@ import { h, createContext, useEffect, useState } from "../deps_frontend.js";
 
 export const AuthContext = createContext({});
 
+const use_token = () => {
+
+    const [ access_token, set_access_token ] = useState(null);
+
+    useEffect(() => {
+
+        const { access_token} = url.parse_hash(window.location);
+        const storage = window.localStorage;
+
+        if (access_token) {
+            
+            storage.setItem("access_token", access_token);
+            set_access_token(access_token);
+        } else {
+
+            const from_storage = storage.get("access_token");
+            set_access_token(from_storage);
+        }
+    }, []);
+
+    return access_token;
+}
+
+const use_user = (access_token, auth0_domain) => {
+
+    const [ user, set_user ] = useState(null);
+
+    useEffect(async () => {
+
+        if (access_token) {
+
+            const response = await http.get(`${auth0_domain}/userinfo`, {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            });
+
+            if (response.status === 200) {
+
+                const user = await response.json();
+                set_user(user);
+            } else {
+
+                console.warn("Had token, but failed to fetch user", response);
+            }
+
+        } else {
+
+            set_user(null);
+        }
+    }, [access_token])
+
+
+    return user; 
+}
+
 /**
  * Handles current authentication status 
  * of applicaxtion user. Available in all children 
@@ -15,29 +71,8 @@ export const AuthContextProvider = props => {
     const { auth0_client_id, auth0_domain, host_uri } = props.auth0; 
     const login_uri = `${auth0_domain}/login?client=${auth0_client_id}&redirect_uri=${host_uri}&response_type=token`;
     
-    
-    const { access_token } = url.parse_hash(window.location);
-
-    const [ user, setUser ] = useState(null);
-
-    useEffect(async () => {
-
-        const response = await http.get(`${auth0_domain}/userinfo`, {
-            headers: {
-                'Authorization': `Bearer ${access_token}`
-            }
-        });
-
-        if (response.status === 200) {
-            
-            const user = await response.json(); 
-            setUser(user);
-
-            const storage = window.localStorage; 
-            storage.setItem("access_token", access_token)
-        }
-    }, [ access_token ])
-
+    const access_token = use_token();
+    const user = use_user(access_token, auth0_domain);
 
     return h`<${AuthContext.Provider} value=${{user, login_uri}}> 
         ${props.children} 
