aa0b449538e6d2edf115229c6b290c3965c15a55
olaven <olav@sundfoer.com>
2020-05-17 15:17:52 +0200

    denon config

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-#export PATH="/Users/olav/.deno/bin:$PATH"
-deno run --allow-net --allow-read --allow-env --allow-write ./app.ts
+#deno run --allow-net --allow-read --allow-env --allow-write ./app.ts
+denon #config in .denon.json
