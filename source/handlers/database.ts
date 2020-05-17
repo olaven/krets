671f59675edade0b9be1213e65d61b5e1dd4d237
olaven <olav@sundfoer.com>
2020-05-17 22:03:53 +0200

    can send responses from frontend

--- source/handlers/database.ts
+++ source/handlers/database.ts
@@ -4,6 +4,12 @@ const users = new Map<string, User>();
 const brands = new Map<string, Brand>()
 const brand_to_response = new Map<string, Response[]>(); 
 
+brands.set("test", {
+    name: "test", 
+    url_name: "test", 
+    owner_id: "google-oauth2|103130415679943370506"
+});
+
 /**
  * Mock database before 
  * I get something "real"
