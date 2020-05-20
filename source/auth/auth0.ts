e0f602a57c1dc71995f50aaf439ef88d4b4a4567
olaven <olav@sundfoer.com>
2020-05-20 16:16:11 +0200

    env variables loaded as uppercase, since action only accepts that

--- source/auth/auth0.ts
+++ source/auth/auth0.ts
@@ -1,11 +1,17 @@
 export const get_auth0 = () => {
 
     const {
-        auth0_client_id, 
-        auth0_client_secret,
-        auth0_domain, 
-        host_uri, 
+        AUTH0_CLIENT_ID, 
+        AUTH0_CLIENT_SECRET,
+        AUTH0_DOMAIN, 
+        HOST_URI, 
     } = Deno.env.toObject();
+
+    //TODO: MAKE UPPERCASE USED IN ENTIRE APPLICATION, replacing this conversion
+    const auth0_client_id = AUTH0_CLIENT_ID; 
+    const auth0_client_secret = AUTH0_CLIENT_SECRET; 
+    const auth0_domain = AUTH0_DOMAIN; 
+    const host_uri = HOST_URI; 
 
     if (!auth0_client_id || !auth0_client_secret || !auth0_domain || !host_uri)
         throw "Auht0 data not properly loaded."
