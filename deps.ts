37f9df88c6d1713e68def4cc25a8044ddac7ea39
olaven <olav@sundfoer.com>
2020-05-22 14:57:30 +0200

    initialized empty next project

--- deps.ts
+++
@@ -1,3 +0,0 @@
-export { App, app, get, post, contentType } from  "https://denopkg.com/olaven/dinatra" // "https://raw.githubusercontent.com/olaven/dinatra/static-files-from-html/mod.ts";//"https://denopkg.com/syumai/dinatra@0.10.1/mod.ts" 
-export { assertEquals, assert, assertNotEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
-export { decode } from "https://deno.land/std/encoding/utf8.ts";
