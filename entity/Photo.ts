a920f47fd6cb183d5e574df35494207a5f57ef7a
olaven <olav@sundfoer.com>
2020-05-23 17:48:27 +0200

    got ts server and psql up and running

---
+++ entity/Photo.ts
@@ -0,0 +1,27 @@
+//require("reflect-metadata");
+import "reflect-metadata"
+import {Entity} = from "typeorm";
+
+@Entity()
+export class Photo {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        length: 100
+    })
+    name: string;
+
+    @Column("text")
+    description: string;
+
+    @Column()
+    filename: string;
+
+    @Column("double")
+    views: number;
+
+    @Column()
+    isPublished: boolean;
+}
