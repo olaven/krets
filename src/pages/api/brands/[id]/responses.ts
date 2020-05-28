c340ffd59b1ab2fdd8b5c338fa063e731f412f23
olaven <olav@sundfoer.com>
2020-05-28 19:48:58 +0200

    First pass at responses endpoint implemetnation

--- src/pages/api/brands/[id]/responses.ts
+++ src/pages/api/brands/[id]/responses.ts
@@ -1,7 +1,22 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import TypeormConnection from "../../../../server/TypeormConnection";
+import {ResponseEntity} from "../../../../server/entities/ResponseEntity";
 
 export default function responsesForBrandInURL(request: NextApiRequest, response: NextApiResponse) {
 
-    console.log("GOING TO FETCH RESPONSES");
-    response.status(501).send("Not implemented");
+    const repository = TypeormConnection.connection.getRepository(ResponseEntity);
+    const { id } = request.query;
+    const responses = repository.createQueryBuilder("response")
+        .where("response.brand.id = :id", { id });
+
+    if (!responses) {
+
+        response
+            .status(404)
+            .send("No repositories found.");
+    } else {
+
+        response
+            .json(responses);
+    }
 }
