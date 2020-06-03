d1281840b8cdbc61f72f2904fef2656e40170a81
olaven <olav@sundfoer.com>
2020-06-03 14:28:47 +0200

    fixed compilatio nerrors

--- src/context/PagesContext.tsx
+++ src/context/PagesContext.tsx
@@ -4,36 +4,40 @@ import {UserContext} from "./UserContext";
 
 const usePages = (uid: string) => {
 
-  const [ pages, setPages ] = useState([]);
+    const [pages, setPages] = useState([]);
 
-  const refreshPages = async () => {
+    const refreshPages = async () => {
 
-    const response = await fetch("/api/pages");
-    if (response.status === 200) {
+        const response = await fetch("/api/pages");
+        if (response.status === 200) {
 
-      const pages = await response.json();
-      console.log("updating pages: ", pages);
-      setPages(pages);
-    } else {
+            const pages = await response.json();
+            console.log("updating pages: ", pages);
+            setPages(pages);
+        } else {
 
-      console.warn(`Received ${response.status} when fetching brands..`);
-    }
-  };
+            console.warn(`Received ${response.status} when fetching brands..`);
+        }
+    };
 
-  useEffect(() => {refreshPages()}, [uid]);
+    useEffect(() => {
+        refreshPages()
+    }, [uid]);
 
-  return [pages, refreshPages];
+    return [pages, refreshPages];
 };
 
-export const PagesContext = createContext({pages: []});
+//TODO: proper types 
+export const PagesContext = createContext({} as any);
+
 export const PagesContextProvider = (props) => {
 
-  const { user } = useContext(UserContext);
-  if (!user) throw "Should not see this if not logged in!";
+    const {user} = useContext(UserContext);
+    if (!user) throw "Should not see this if not logged in!";
 
-  const [pages, refreshPages] = usePages(user.sub);
+    const [pages, refreshPages] = usePages(user.sub);
 
-  return <PagesContext.Provider value={{pages, refreshPages}}>
-      {props.children}
-  </PagesContext.Provider>
+    return <PagesContext.Provider value={{pages, refreshPages}}>
+        {props.children}
+    </PagesContext.Provider>
 };
