fbe7fe916f4ad6d4d7d3d9ffa7e1313482258d78
olaven <olav@sundfoer.com>
2020-06-06 14:06:35 +0200

    node pg 8 and tweaked ssl options

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -5,18 +5,16 @@ import path from "path";
 
 //TODO: keep config out of code (i.e not programattic check for test)
 
-path.join(__dirname, '..', 'config', 'dev', 'foobar.json');
-const config = process.env.NODE_ENV == "test"?
-    {
-        // this object will be passed to the TLSSocket constructor
-        ssl: {
-            ca: fs.readFileSync(path.resolve(__dirname, "certificate.crt")).toString(),
-            /*        key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
-                    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),*/
-            rejectUnauthorized : false,
-        },
-    }:
-    {};
+//path.join(__dirname, '..', 'config', 'dev', 'foobar.json');
+const config = process.env.NODE_ENV !== "test"?{
+
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(path.join(__dirname, './certificate.crt')).toString(),
+  //      key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+  //    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString()
+    }
+}:{};
 
 const pool = new Pool(config);
 
