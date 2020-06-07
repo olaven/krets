028079703b2ef7081e024383694b8be4390c72cf
olaven <olav@sundfoer.com>
2020-06-07 22:50:40 +0200

    ui tweaks in code page

--- src/pages/[pageId]/code.tsx
+++ src/pages/[pageId]/code.tsx
@@ -1,22 +1,29 @@
-import {useRouter} from "next/router";
-import {QRCode} from "react-qrcode-logo";
+import { useRouter } from "next/router";
+import { QRCode } from "react-qrcode-logo";
 import React from "react";
-import {Box, Flex, Heading} from "rebass";
+import { Box, Flex, Heading } from "rebass";
+import { usePage } from "../../effects/usePage";
 
 export default () => {
 
     const router = useRouter();
-    const {pageId} = router.query;
+    const { pageId } = router.query;
     //TODO: use pagecontext to pull correct page 
     const pageLink = `https://krets.app/${pageId}`;
+    const [page, _] = usePage(pageId as string);
+
+    const headingText = page ?
+        `Gi tilbakemelding til ${page.name}`:
+        `(Laster..)`;
+
 
     return <Box m={"auto"} py={[4, 8, 16]}>
-        <Box>
-            <Heading color={"primary"}>Gi tilbakemelding!</Heading>
-        </Box>
-        <Box>
-            <QRCode value={pageLink} enableCORS={false}/>
-        </Box>
+        <Flex m={"auto"}>
+            <Box m={"auto"} style={{textAlign: "center"}}>
+                <Heading m="auto" color={"primary"}>{headingText}</Heading>
+                <QRCode value={pageLink} enableCORS={false} />
+            </Box>
+        </Flex>
 
     </Box>
 }
