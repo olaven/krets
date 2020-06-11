2b09cd716ec45659640b84a8a686e46bac5fc106
olaven <olav@sundfoer.com>
2020-06-11 21:56:11 +0000

    fetch mock worked after switching to `mount`. FIXME: pagesection never rendered, even if user is defined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {UserContext} from "../context/UserContext";
-import {PageSection} from "../components/Home/PageSection/PageSection";
-import {IntroSection} from "../components/Home/IntroSection/IntroSection";
+import { UserContext } from "../context/UserContext";
+import { PageSection } from "../components/Home/PageSection/PageSection";
+import { IntroSection } from "../components/Home/IntroSection/IntroSection";
 
 const IndexPage = () => {
 
     const { user } = React.useContext(UserContext);
 
-    return user?
-        <PageSection/>:
-        <IntroSection/>
+    return user ?
+        <PageSection /> :
+        <IntroSection />
 };
 
 export default IndexPage
