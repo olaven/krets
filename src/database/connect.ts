834c1ed538b4322de572da0672623f48a5248f06
olaven <olav@sundfoer.com>
2020-06-16 16:29:33 +0000

    FIX: using ca value in config after accidentally removing it

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -7,39 +7,14 @@ const config = (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "dev
         ca: process.env.SSH_DATABASE_CERTIFICATE,
     }
 };
-const pgPool = new Pool({
+const pool = new Pool({
     user: 'user',
     database: 'data',
     password: 'pass',
+    ...config
 })
 
-//const pgPool = new Pool();
-const pgPoolWrapper = {
-    async connect() {
-        for (let nRetry = 1; ; nRetry++) {
-            try {
-                const client = await pgPool.connect();
-                if (nRetry > 1) {
-                    console.info('Now successfully connected to Postgres');
-                }
-                return client;
-            } catch (e) {
-                if (e.toString().includes('ECONNREFUSED') && nRetry < 5) {
-                    console.info('ECONNREFUSED connecting to Postgres, ' +
-                        'maybe container is not ready yet, will retry ' + nRetry);
-                    // Wait 1 second
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                } else {
-                    throw e;
-                }
-            }
-        }
-    }
-};
-//console.log('Database config object', config);
-//const pool = new Pool(config);
-
-pgPool.on('error', (err, client) => {
+pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err);
     process.exit(-1)
 });
@@ -48,7 +23,7 @@ pgPool.on('error', (err, client) => {
 
 export const withDatabase = async <T>(action: (client: PoolClient) => Promise<T>) => {
 
-    const client = await pgPoolWrapper.connect();
+    const client = await pool.connect();
     const result = await action(client);
     client.release();
 
