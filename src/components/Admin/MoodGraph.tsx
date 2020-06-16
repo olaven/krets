b67a4c24aa4610f6de725cb6dc9e2b30d412f890
olaven <olav@sundfoer.com>
2020-06-16 15:10:05 +0000

    basic graph is shown with data from db

--- src/components/Admin/MoodGraph.tsx
+++ src/components/Admin/MoodGraph.tsx
@@ -20,55 +20,31 @@ const emotionToNumeric = (emotion: Emotion) => ({
     ":-(": 0,
 }[emotion])
 
-const toChartCoordinates = (responses: ReseponseModel[]) =>
-    responses.map(({ emotion, created_at }) => ({
-        x: daysSince(created_at),
-        y: emotionToNumeric(emotion),
-    }))
+const averageUntil = (response: ReseponseModel, responses: ReseponseModel[]) => {
+
+    const date = new Date(response.created_at);
+    const relevant = responses
+        .sort((a, b) => new Date(a.created_at) < new Date(b.created_at) ? -1 : 1)
+        .filter(({ created_at }) => new Date(created_at).getTime() <= date.getTime())
+
+    const sum = relevant.length === 1 ?
+        emotionToNumeric(relevant[0].emotion) :
+        relevant
+            .map(({ emotion }) => emotionToNumeric(emotion))
+            .reduce((a, b) => a + b)
+
+    return ({
+        y: sum / relevant.length,
+        x: -1 * daysSince(response.created_at)
+    })
+}
+
+const averageOverTime = (responses: ReseponseModel[]) => responses
+    .map(response => averageUntil(response, responses));
 
 export const MoodGraph = () => {
 
-    //const { responses, responsesLoading } = useContext(AdminPageContext);
-
-    const today = new Date()
-    const responses: ReseponseModel[] = [
-        {
-            emotion: ":-)",
-            text: "This was good",
-            page_id: "1",
-            created_at: today.toString()
-        },
-        {
-            emotion: ":-)",
-            text: "This was good",
-            page_id: "2",
-            created_at: today.toString()
-        },
-        {
-            emotion: ":-|",
-            text: "This was ok",
-            page_id: "3",
-            created_at: new Date(today.getTime() - (1000 * 60 * 60 * (24 * 3.15))).toString()
-        },
-        {
-            emotion: ":-|",
-            text: "Not what I expected",
-            page_id: "4",
-            created_at: new Date(today.getTime() - (1000 * 60 * 60 * (24 * 4.4))).toString()
-        },
-        {
-            emotion: ":-)",
-            text: "Not what I expected",
-            page_id: "5",
-            created_at: new Date(today.getTime() - (1000 * 60 * 60 * (24 * 2.2))).toString()
-        },
-        {
-            emotion: ":-)",
-            text: "Not what I expected",
-            page_id: "6",
-            created_at: new Date(today.getTime() - (1000 * 60 * 60 * (24 * 4.1))).toString()
-        }
-    ];
+    const { responses, responsesLoading } = useContext(AdminPageContext);
 
     /* const EmojiPoint = (props) => {
 
@@ -85,27 +61,11 @@ export const MoodGraph = () => {
             {getEmoji()}
         </text>
     } */
-    const averageUpTo = (response: ReseponseModel, responses: ReseponseModel[]) => {
 
-        const date = new Date(response.created_at);
-        const relevant = responses
-            .sort((a, b) => new Date(a.created_at) < new Date(b.created_at) ? -1 : 1)
-            .filter(({ created_at }) => new Date(created_at).getTime() < date.getTime())
+    if (responses.length <= 1) {
 
-        const sum = relevant
-            .map(({ emotion }) => emotionToNumeric(emotion))
-            .reduce((a, b) => a + b)
-
-        return ({
-            y: sum / relevant.length,
-            x: -1 * daysSince(response.created_at)
-        })
+        return <div>Motta mer respons for å vise graf.</div>
     }
-
-    //TODO: test this 
-    const averageOverTime = (responses: ReseponseModel[]) => responses
-        .map(response => averageUpTo(response, responses));
-
     return <div>
         <div>
             Samlet utvikling
@@ -119,6 +79,8 @@ export const MoodGraph = () => {
                 <VictoryArea
                     /* dataComponent={<EmojiPoint />} */
                     data={averageOverTime(responses)} style={{ data: { fill: "orange", opacity: 0.7 } }} />
+                <VictoryAxis />
+                <VictoryAxis />
             </VictoryChart>
         </div>
     </div>
