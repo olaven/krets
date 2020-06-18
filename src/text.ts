a53f5bac23c01c8e29a3fdb8b42deb0af7d08599
olaven <olav@sundfoer.com>
2020-06-18 21:04:51 +0000

    localized text on intro page (TODO: images)

---
+++ src/text.ts
@@ -0,0 +1,23 @@
+import LocalizedStrings from "react-localization";
+
+export const intro = new LocalizedStrings({
+    en: {
+        create: "Creating a page on Krets is fast!",
+        visiting: "Your audience visits your site, with a link or a QR-code",
+        insight: "Gain insight!"
+    },
+    no: {
+        create: "Å lage en Krets-side er kjapt!",
+        visiting: "Ditt publikum besøker din side, med link eller QR-kode",
+        insight: "Få innsikt!"
+    }
+})
+
+export const text = new LocalizedStrings({
+    en: {
+        frictionless: "frictionless feedback",
+    },
+    no: {
+        frictionless: "friksjonsfri tilbakemelding"
+    }
+});
