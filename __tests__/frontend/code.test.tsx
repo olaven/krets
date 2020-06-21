1870fc67335092827a109b432e1cb2eb49011674
olaven <olav@sundfoer.com>
2020-06-21 15:25:02 +0200

    100% coverage in code.tsx

--- __tests__/frontend/code.test.tsx
+++ __tests__/frontend/code.test.tsx
@@ -4,32 +4,62 @@
 
 import React from "react";
 import CodePage from "../../src/pages/[pageId]/code"
-import Enzyme, { render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { RouterContext } from 'next-server/dist/lib/router-context';
 import * as nextRouter from 'next/router';
-import { UserContext, UserContextProvider } from "../../src/context/UserContext";
-import { PagesContextProvider } from "../../src/context/PagesContext";
-import { createRouter } from 'next/router';
-import { users } from "../../src/database/users";
+import { waitFor, render } from "@testing-library/react"
+import { PageModel } from "../../src/models";
+import '@testing-library/jest-dom/extend-expect'
 
 
 
-Enzyme.configure({ adapter: new Adapter() });
+//Enzyme.configure({ adapter: new Adapter() });
 
 
-describe("The QR/code page", () => {
-
+const mockRouter = (pageId: string) => {
     //@ts-ignore
     nextRouter.useRouter = jest.fn();
     //@ts-ignore
-    nextRouter.useRouter.mockImplementation(() => ({ query: {
-        pageId: "some mock pageid"
-    } }));
+    nextRouter.useRouter.mockImplementation(() => ({
+        query: { pageId }
+    }));
+}
+
+//TODO: use in index.test.tsx as well (shared)
+const mockGet = <T extends unknown>(payload: T) => {
+
+    global.fetch = jest.fn(() => {
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve(payload)
+        } as Response);
+    });
+}
+
+describe("The QR/code page", () => {
+
+    //NOT relevant, not waiting for result 
+    mockGet({})
 
     it("'loading' while waiting for page", async () => {
 
-            const component = render(<CodePage />);
-            expect(component.text()).toContain("Loading"); 
+        mockRouter("test-id");
+        const { container, getByText } = render(<CodePage></CodePage>)
+        expect(getByText("(Loading...)")).toBeInTheDocument()
     });
+
+    it("Shows name of the specified page", async () => {
+
+        const mockPage: PageModel = {
+            id: "mock-test-page",
+            name: "Mocked Test Page",
+            owner_id: "owner_id"
+        }
+
+        mockRouter(mockPage.id);
+        mockGet(mockPage);
+
+        const { getByText } = render(<CodePage />);
+        await waitFor(() => {
+            expect(getByText(`Scan to give feedback to ${mockPage.name}`)).toBeInTheDocument();
+        })
+    })
 });  
