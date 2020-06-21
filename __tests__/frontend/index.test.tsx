d1925a9a50128be5d9a876cdd02dcf90c9ec0765
olaven <olav@sundfoer.com>
2020-06-21 23:15:49 +0200

    moved away from enzyme

--- __tests__/frontend/index.test.tsx
+++ __tests__/frontend/index.test.tsx
@@ -4,57 +4,46 @@
 
 import React from "react";
 import IndexPage from "../../src/pages/index"
-import Enzyme, { render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { waitFor, render } from "@testing-library/react"
 import { AuthModel } from "../../src/models";
 import * as faker from "faker";
+import '@testing-library/jest-dom/extend-expect'
 import { UserContext, UserContextProvider } from "../../src/context/UserContext";
 import { PagesContextProvider } from "../../src/context/PagesContext";
-Enzyme.configure({ adapter: new Adapter() });
+import * as text from "../../src/text"
+import { mockGet } from "./frontedTestUtils";
+import { stringify } from "querystring";
 
 describe("The home page", () => {
 
 
-    it("renders something", () => {
-
-        const wrapper = mount(<IndexPage />);
-        expect(wrapper.text()).toBeTruthy();
-    });
-
     it("Shows intro section when no user is present", () => {
 
-        const wrapper = mount(<IndexPage />);
-        expect(wrapper.text()).toContain('Creating a page on Krets is fast!'); //TODO: have som global (wiht translation) text store
+        const rendered = render(<IndexPage />)
+        waitFor(() => {
+
+            expect(rendered.findByText(text.intro.create)).toBeInTheDocument();
+        })
     });
 
+    /* 
+        it("Shows pages when user _is_ present", async () => {
     
-    it("Shows pages when user _is_ present", async () => {
-
-        const mockUser: AuthModel = {
-            sub: faker.random.uuid(),
-            name: faker.name.firstName()
-        }
-
-        global.fetch = jest.fn(() => {
-            return Promise.resolve({
-                status: 200,
-                json: () => Promise.resolve(mockUser)
-            } as Response);
-        });
-
-        const wrapper = mount(<UserContextProvider>
-            <PagesContextProvider user={mockUser}>
-                <IndexPage />
-            </PagesContextProvider>
-        </UserContextProvider>);
-
-        await wrapper.update();
-
-        //TODO: Try to mock child component (PageSection), as we really just want to test that index.tsx actually renders it
-        //FIXME: this test does not work, as the state is not yet updated. 
-        //expect(wrapper.text()).not.toContain("Information");
-        //expect(wrapper.text()).toContain("Dine sider!");
-
-        delete global.fetch;
-    })
+            const mockUser: AuthModel = {
+                sub: faker.random.uuid(),
+                name: faker.name.firstName()
+            }
+    
+            mockGet(mockUser);
+    
+            const { findByText } = render(<UserContextProvider>
+                <PagesContextProvider user={mockUser}>
+                    <IndexPage />
+                </PagesContextProvider>
+            </UserContextProvider>);
+    
+            waitFor(() => {
+                expect(findByText(text.intro.create)).not.toBeInTheDocument();
+            })
+        }) */
 });
