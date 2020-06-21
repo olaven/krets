49df03238cefe4f656c22f98cef42d4579f93622
olaven <olav@sundfoer.com>
2020-06-21 16:05:54 +0200

    WIP: admin tests

---
+++ __tests__/frontend/admin.test.tsx
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { AdminPage } from "../../src/components/Admin/AdminPage"
+import { averageUntil, emotionToNumeric } from "../../src/components/Admin/MoodGraph"
+import { render } from "@testing-library/react"
+import { ReseponseModel } from "../../src/models";
+import * as text from "../../src/text"
+import '@testing-library/jest-dom/extend-expect'
+import * as faker from "faker";
+
+const fakeResponses = (amount: number): ReseponseModel[] => new Array(amount)
+    .map(() => ({
+        id: faker.random.uuid(),
+        emotion: ":-)", //TODO: make random
+        text: faker.lorem.lines(3),
+        page_id: faker.random.uuid(),
+        created_at: faker.date.past().getTime().toString(),
+    }))
+
+describe("Admin page", () => {
+
+    it("Displays message about loading", () => {
+
+        const { getByText } = render(<AdminPage />);
+        expect(getByText(text.adminPage.loading)).toBeInTheDocument();
+    });
+});
+
+describe("Mood graph", () => {
+
+    describe("Calculation of coordinates", () => {
+
+        it("Returns Y coordinate with average", () => {
+
+            const n = faker.random.number(10);
+
+            const responses = fakeResponses(n);
+            const expected = (fakeResponses(n)
+                .map(response => response.emotion)
+                .map(emotionToNumeric)
+                .reduce((a, b) => a + b)) / responses.length
+
+            const result = averageUntil(responses[responses.length - 1], responses);
+
+            expect(result.y).toEqual(expected);
+        })
+    });
+}); 
