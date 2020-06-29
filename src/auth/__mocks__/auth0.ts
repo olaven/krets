01f949351204b32d3b67d565d130cd98280356a8
olaven <olav@sundfoer.com>
2020-06-29 11:29:52 +0200

    removed test config in order for it to be overridden by master

--- src/auth/__mocks__/auth0.ts
+++ src/auth/__mocks__/auth0.ts
@@ -1,9 +1,9 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {LoginOptions} from "@auth0/nextjs-auth0/dist/handlers/login";
-import {CallbackOptions} from "@auth0/nextjs-auth0/dist/handlers/callback";
-import {ProfileOptions} from "@auth0/nextjs-auth0/dist/handlers/profile";
-import {ISession} from "@auth0/nextjs-auth0/dist/session/session";
-import {IApiRoute} from "@auth0/nextjs-auth0/dist/handlers/require-authentication";
+import { NextApiRequest, NextApiResponse } from "next";
+import { LoginOptions } from "@auth0/nextjs-auth0/dist/handlers/login";
+import { CallbackOptions } from "@auth0/nextjs-auth0/dist/handlers/callback";
+import { ProfileOptions } from "@auth0/nextjs-auth0/dist/handlers/profile";
+import { ISession } from "@auth0/nextjs-auth0/dist/session/session";
+import { IApiRoute } from "@auth0/nextjs-auth0/dist/handlers/require-authentication";
 import { UNAUTHORIZED } from "../../http/codes";
 
 
@@ -30,7 +30,7 @@ export default ({
 
         let session = await getSession(req);
         if (options && options.onUserLoaded) {
-            await options.onUserLoaded(req, res, session, {mock: "STATE"});
+            await options.onUserLoaded(req, res, session, { mock: "STATE" });
         }
 
         res.end();
