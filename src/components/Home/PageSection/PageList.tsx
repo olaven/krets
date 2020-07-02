6f43532f16cc1c363a371dd2db5e989234f5f68a
olaven <olav@sundfoer.com>
2020-07-02 11:42:44 +0000

    elements in list are now slightly hovering cards

--- src/components/Home/PageSection/PageList.tsx
+++ src/components/Home/PageSection/PageList.tsx
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Box, Card, Flex, Heading } from "rebass";
 import { ToAdmin, ToQR, ToPage } from "../../tiny/buttons";
 import { PagesContext } from "../../../context/PagesContext";
 import * as text from "../../../text"
 
-const PageCard = ({ id, name, responses }) => <Box>
-    <Card sx={{ borderTopStyle: "solid", borderColor: "primary", borderWidth: "1px" }}>
+const PageCard = ({ id, name }) => <Box>
+    <Card sx={{ boxShadow: "0px 10px 20px .25px grey" }} p={[0, 1, 2]} my={[0, 1, 2]}>
 
         <Heading mx={[1, 2, 3]} mt={[1, 2, 3]} fontSize={[3, 4, 5]}>{name}</Heading>
         <Flex mb={[1, 2, 3]} flexWrap={"wrap"}>
