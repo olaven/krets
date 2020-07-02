36ec80f34bb741c826659df2c87cb895a71c4049
olaven <olav@sundfoer.com>
2020-07-02 08:38:51 +0000

    listbuttson now show text instead of icons

--- src/components/tiny/buttons.tsx
+++ src/components/tiny/buttons.tsx
@@ -30,21 +30,22 @@ export const GetStartedButton = () => <Flex width={1} py={[0, 1, 2]}>
 </Flex>
 
 
-const ListButton = (href: string, icon: string) => <Box mx={[0, 1, 2]}>
+const ListButton = (href: string, text: string) => <Box mx={[0, 1, 2]}>
     <Button p={[0, 1, 2]}>
         <Link color={"secondary"} href={href}>
-            <span className="material-icons">
+            {/* <span className="material-icons">
                 {icon}
-            </span>
+            </span> */}
+            {text}
         </Link>
     </Button>
 </Box>;
 
 export const ToAdmin = ({ id }) =>
-    ListButton(`/${id}/admin`, "trending_up");
+    ListButton(`/${id}/admin`, "Mine tilbakemeldinger" /* "trending_up" */);
 
 export const ToQR = ({ id }) =>
-    ListButton(`/${id}/code`, "qr_code");
+    ListButton(`/${id}/code`, "QR-kode" /* "qr_code" */);
 
 export const ToPage = ({ id }) =>
-    ListButton(`/${id}`, "reply")
+    ListButton(`/${id}`, "Tilbakemeldingsside" /* "reply" */);
