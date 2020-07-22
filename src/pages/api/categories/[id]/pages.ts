c0c74526f06c4a4117bfda711aca80ea7656201d
olaven <olav@sundfoer.com>
2020-07-22 10:37:22 +0000

    Implemented /categories/[id]/pages. TODO: test

--- src/pages/api/categories/[id]/pages.ts
+++ src/pages/api/categories/[id]/pages.ts
@@ -1,9 +1,15 @@
-import auth0 from "../../../../../auth0";
 import { useRouter } from "next/router";
+import auth0 from "../../../../../auth0";
+import { pages } from "../../../../../src/database/pages"
+import { OK } from "../../../../http/codes";
 
 export default auth0.requireAuthentication(async (request, response) => {
 
+    const { user } = await auth0.getSession(request);
+    const id = useRouter().query.id as string
 
-    const { query: id } = useRouter();
-
-}
+    const retrieved = pages.getByOwnerAndCategory(user.sub, id)
+    response
+        .status(OK)
+        .send(retrieved);
+}); 
