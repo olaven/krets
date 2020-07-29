c7d84de8f77e077a3fdc7981f25f1bb383ff24aa
olaven <olav@sundfoer.com>
2020-07-29 17:53:49 +0000

    Removed auth handler duplication, as per Codebeat's warning

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,13 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
 import auth0 from '../../../auth/auth0';
-import { BAD_REQUEST } from 'node-kall';
+import { auth0HandleWrapper } from './logout';
 
-export default async function login(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        await auth0.handleLogin(req, res);
-    } catch (error) {
-
-        console.error(error);
-        res.status(error.status || BAD_REQUEST).end(error.message);
-    }
-}
+export default auth0HandleWrapper(auth0.handleLogin); 
