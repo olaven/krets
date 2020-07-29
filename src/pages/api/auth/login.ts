16ff55ce922764a990bc0300ab683095043f9df3
olaven <olav@sundfoer.com>
2020-07-29 16:45:47 +0000

    Remove swallowing of error no longer relevant

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,4 +1,4 @@
-import next, { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 import auth0 from '../../../auth/auth0';
 import { BAD_REQUEST } from 'node-kall';
 
@@ -6,13 +6,6 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
     try {
         await auth0.handleLogin(req, res);
     } catch (error) {
-
-        if (error.errorMessage.includes("Cannot find module 'next/dist/next-server/server/node-polyfill-fetch'")) {
-
-            //TODO: temp. fix on inexplainable crash
-            res.status(200).send("Ignored this error <3");
-            return;
-        }
 
         console.error(error);
         res.status(error.status || BAD_REQUEST).end(error.message);
