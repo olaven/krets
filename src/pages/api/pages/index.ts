bc9b6963a16623eb8bd964194039bb8dfcc320be
olaven <olav@sundfoer.com>
2020-07-30 16:48:05 +0000

    Attempted to make pages/index.ts more readable

--- src/pages/api/pages/index.ts
+++ src/pages/api/pages/index.ts
@@ -3,41 +3,51 @@ import auth0 from "../../../auth/auth0";
 import { pages } from "../../../database/database";
 import { CREATED, OK } from "node-kall";
 import { PageModel } from "../../../models";
+import { NextApiResponse, NextApiRequest } from "next";
+
+const get = async (request: NextApiRequest, response: NextApiResponse) => {
+
+    const { user } = await auth0.getSession(request);
+
+    const pagesInDatabase = await pages.getByOwner(user.sub);
+
+    response
+        .status(OK)
+        .json(pagesInDatabase);
+}
+
+const post = async (request: NextApiRequest, response: NextApiResponse) => {
+
+    const { user } = await auth0.getSession(request);
+
+    const page = request.body as PageModel;
+    page.owner_id = user.sub;
+    page.color = randomColor();
+
+    try {
+
+        const result = await pages.createPage(page);
+
+        response
+            .status(CREATED)
+            .json(result)
+    } catch (error) {
+
+        console.log("PageId iwht error: ", page, "error", error);
+        throw error
+    }
+
+}
 
 
 export default auth0.requireAuthentication(async function pagesHandler(request, response) {
 
-    const { user } = await auth0.getSession(request);
-
     if (request.method === "GET") {
 
-        const pagesInDatabase = await pages.getByOwner(user.sub);
-
-        response
-            .status(OK)
-            .json(pagesInDatabase);
-
+        get(request, response);
     } else if (request.method === "POST") {
 
-        //NOTE: automatically set page owner
-        const page = request.body as PageModel;
-        page.owner_id = user.sub;
-        page.color = randomColor();
-
-        try {
-
-            const result = await pages.createPage(page);
-
-            response
-                .status(CREATED)
-                .json(result)
-        } catch (error) {
-
-            console.log("PageId iwht error: ", page, "error", error);
-            throw error
-        }
-
-
+        post(request, response);
     }
 });
 
