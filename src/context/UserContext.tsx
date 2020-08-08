ab303b30ed249a033465f76931a21f89cc4bce65
olaven <olav@sundfoer.com>
2020-08-08 15:50:01 +0000

    Added email to auth0-scopes. Handy for mail.ts and payments, for which it is required

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -20,6 +20,7 @@ export const UserContextProvider = props => {
         const [status, user] = await get<AuthModel>('/api/auth/me');
         if (status === 200) {
 
+            console.log(user);
             setUser(user);
         } else {
 
