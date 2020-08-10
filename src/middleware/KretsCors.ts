a99ca8f506c4d6b02f4a7378c80395744e12ee03
olaven <olav@sundfoer.com>
2020-08-10 17:56:47 +0000

    Back to micro-cors. Want to test in deploye environment

--- src/middleware/KretsCors.ts
+++ src/middleware/KretsCors.ts
@@ -1,41 +1,13 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-import { initMiddleware } from "next/"
-import Cors from 'cors'
+import { NextApiHandler } from "next";
+import Cors from 'micro-cors'
 
 //FIXME: does it acually do something? 
 const cors = Cors({
-    methods: ['HEAD'],
     origin: process.env.NODE_ENV === "production" ?
         "https://krets.app" :
         "localhost"
 });
 
 
-/* const runMiddleware = async (request, response, middleware) => {
-
-    middleware(request, response, result => {
-        if (result instanceof Error) {
-            throw result;
-        }
-        return result;
-    })
-} */
-
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, result => {
-            if (result instanceof Error) {
-                return reject(result)
-            }
-
-            return resolve(result)
-        })
-    })
-}
-
 export const KretsCors = (handler: NextApiHandler) =>
-    async (request: NextApiRequest, response: NextApiResponse) => {
-
-        await runMiddleware(request, response, cors);
-        handler(request, response);
-    }
+    cors(handler)
