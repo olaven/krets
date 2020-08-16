245c0a623fbd6bc87d73d261a21bfaaf034c63b5
olaven <olav@sundfoer.com>
2020-08-16 20:28:57 +0000

    WIP: line chart fetching coordinate sfrom server

--- src/components/Admin/Charts/LineChart.tsx
+++ src/components/Admin/Charts/LineChart.tsx
@@ -1,51 +1,39 @@
-import { ResponseModel, PageModel } from "../../../models";
 import { VictoryChart, VictoryLabel, VictoryLine, VictoryAxis } from "victory";
-import { emotionToNumeric } from "./ChartUtils";
 import { PageInformation } from "../../../context/CompareContext";
-import { response } from "../../../text";
+import { OK } from "node-kall"
+import { getLineCoordinates } from "../../../fetchers";
+import { useState } from "react";
+import { asyncEffect } from "../../../effects/useProducts";
 
-const getRelevant = (response: ResponseModel, responses: ResponseModel[]) =>
-    responses.filter(r => new Date(r.created_at).getTime() <= new Date(response.created_at).getTime());
 
-//TODO: this should be done in an SQL query backend 
-const sumEmotions = (responses: ResponseModel[]) => responses
-    .map(({ emotion }) => emotionToNumeric(emotion))
-    .reduce((a, b) => a + b)
+const PageLine = ({ page }) => {
 
-const isLast = (element: any, array: any[]) =>
-    array.indexOf(element) === (array.length - 1)
+    const [coordinates, setCoordinates] = useState([]);
 
-const toDateCoordinates = (page: PageModel, response: ResponseModel, responses: ResponseModel[]) => {
+    asyncEffect(async () => {
 
-    const relevant = getRelevant(response, responses);
-    const sum = sumEmotions(relevant);
-    const average = sum / relevant.length;
+        const [status, coordinates] = await getLineCoordinates(page.id);
+        console.log("Fetched seomthing, ", coordinates);
+        if (status === OK) {
 
-    return {
-        y: average,
-        x: new Date(response.created_at),
-        label: isLast(response, responses) ?
-            page.name : null
-    }
+            setCoordinates(coordinates);
+        } else {
+
+            console.error(`${status} when fetching line coordinates..`);
+        }
+    }, []);
+
+    console.log("coordinates rendering: ", coordinates)
+    return <VictoryLine
+        name={`line_${page.id}`}
+        style={{
+            data: { stroke: page.color ? page.color : "cyan", strokeWidth: 5 }
+        }}
+        data={coordinates}
+        labelComponent={<VictoryLabel dx={10} dy={15} renderInPortal />}
+        interpolation={"natural"}
+    />
 }
-
-const toLines = (pageInformations: PageInformation[]) => pageInformations.map(({ page, responses }) => {
-
-    const chartData = responses
-        .map(response => toDateCoordinates(page, response, responses))
-
-    return (
-        <VictoryLine
-            key={page.id}
-            name={`line_${page.id}`}
-            style={{
-                data: { stroke: page.color ? page.color : "cyan", strokeWidth: 5 } //TODO: page.color
-            }}
-            data={chartData}
-            labelComponent={<VictoryLabel dx={10} dy={15} renderInPortal />}
-        />
-    );
-});
 
 export const LineChart = ({ pageInformations }: { pageInformations: PageInformation[] }) => <span
     aria-label="line-chart-label">
@@ -58,7 +46,12 @@ export const LineChart = ({ pageInformations }: { pageInformations: PageInformat
             tickValues={[0, 1, 2]}
             tickFormat={tick => [":-(", ":-|", ":-)"][tick]} //TODO: Proper emoji
         />
-        {toLines(pageInformations)}
+
+        {pageInformations.map(({ page }) =>
+            <PageLine key={page.id} page={page} />
+        )}
+        {/* {pageInformations.map(({ page }) =>
+            <PageLine key={page.id} page={page} />
+        )} */}
     </VictoryChart>
 </span>
-
