c1fd6504d92e610c04ce90458c1cfac4a8070426
olaven <olav@sundfoer.com>
2020-08-17 20:55:15 +0000

    linechart-client now fully depends on server for calculation

--- src/components/Admin/Charts/LineChart.tsx
+++ src/components/Admin/Charts/LineChart.tsx
@@ -1,65 +1,49 @@
 import { VictoryChart, VictoryLabel, VictoryLine, VictoryAxis } from "victory";
-import { useEffect, useState } from "react";
-import { OK } from "node-kall"
+import { useState } from "react";
+import { filterBody } from "node-kall"
 import { PageInformation } from "../../../context/CompareContext";
 import { getLineCoordinates } from "../../../fetchers";
 import { asyncEffect } from "../../../effects/asyncEffect";
 
 
-const PageLine = ({ page }) => {
+export const LineChart = ({ pageInformations }: { pageInformations: PageInformation[] }) => {
 
-    const [coordinates, setCoordinates] = useState([]);
+    const [pageWithCoordinates, setPageWithCoordinates] = useState([]);
 
     asyncEffect(async () => {
 
-        const [status, coordinates] = await getLineCoordinates(page.id);
-        console.log("Fetched seomthing, ", coordinates);
-        if (status === OK) {
+        const pageWithCoordinates = await Promise.all(
+            pageInformations.map(({ page }) => page)
+                .map(async page => ({ page, coordinates: await filterBody(getLineCoordinates(page.id)) }))
+        )
 
-            setCoordinates(coordinates);
-        } else {
+        setPageWithCoordinates(pageWithCoordinates);
+    }, [pageInformations.length]);
 
-            console.error(`${status} when fetching line coordinates..`);
-        }
-    }, []);
+    console.log(pageWithCoordinates);
 
-    /*
-        NOTE: Lines not getting updated. 
-        I have posted [an issue](https://github.com/FormidableLabs/victory/issues/1667). 
-        Waiting for awhile, seeing if the Victory-maintainer sees something I don't. 
-    */
-    console.log("GOing for coordinatse", coordinates);
-    return coordinates.length === 0 ?
-        null :
-        <VictoryLine
-            name={`line_${page.id}`}
-            style={{
-                data: { stroke: page.color ? page.color : "cyan", strokeWidth: 5 }
-            }}
-            data={coordinates}
-            labelComponent={< VictoryLabel dx={10} dy={15} renderInPortal />}
-            interpolation={"natural"}
-        />
+    return <span
+        aria-label="line-chart-label">
+        <VictoryChart
+            domainPadding={{ y: 10 }}
+            domain={{ y: [0, 2] }}
+        >
+            <VictoryAxis
+                dependentAxis
+                tickValues={[0, 1, 2]}
+                tickFormat={tick => [":-(", ":-|", ":-)"][tick]} //TODO: Proper emoji
+            />
+
+            {pageWithCoordinates.map(({ page, coordinates }) => <VictoryLine
+                name={`line_${page.id}`}
+                style={{
+                    data: { stroke: page.color ? page.color : "cyan", strokeWidth: 5 }
+                }}
+                data={coordinates}
+                labelComponent={< VictoryLabel dx={10} dy={15} renderInPortal />}
+                interpolation={"natural"}
+            />)}
+        </VictoryChart>
+    </span>
 
 }
-
-export const LineChart = ({ pageInformations }: { pageInformations: PageInformation[] }) => <span
-    aria-label="line-chart-label">
-    <VictoryChart
-        domainPadding={{ y: 10 }}
-        domain={{ y: [0, 2] }}
-    >
-        <VictoryAxis
-            dependentAxis
-            tickValues={[0, 1, 2]}
-            tickFormat={tick => [":-(", ":-|", ":-)"][tick]} //TODO: Proper emoji
-        />
-
-        {pageInformations.map(({ page }) =>
-            <PageLine key={page.id} page={page} />
-        )}
-        {/* {pageInformations.map(({ page }) =>
-            <PageLine key={page.id} page={page} />
-        )} */}
-    </VictoryChart>
-</span>
