dd6250dd3e6dc6445d5940213ca4779c10b33dfb
olaven <olav@sundfoer.com>
2020-08-18 11:30:37 +0000

    Tweaked respons text list design, addressing #167

--- src/components/Admin/TextList/TextCard.tsx
+++ src/components/Admin/TextList/TextCard.tsx
@@ -1,7 +1,8 @@
-import { Card, Flex, Text } from "rebass";
+import { Card, Box, Flex, Text } from "rebass";
 import Emoji from "react-emoji-render";
 import { ResponseModel } from "../../../models";
 import * as text from "../../../text";
+
 
 const formatDate = (dateString: string) => {
 
@@ -12,19 +13,18 @@ const formatDate = (dateString: string) => {
     const year = date.getFullYear();
 
     const currentYear = new Date().getFullYear();
-    return `${day}/${month}${
-        currentYear !== year ?
-            `/${year}` : ``
+    return `${day}/${month}${currentYear !== year ?
+        `/${year}` : ``
         }`
 }
 
 
 export const TextCard = ({ response }: { response: ResponseModel }) =>
-    <Card p={[0, 1, 2]} m={[0, 1, 2]} backgroundColor={"primary"} color="secondary">
+    <Card p={[0, 1, 2]} m={[0, 1, 2]} backgroundColor={"primary"} color={"secondary"}>
         <Flex flexDirection="column">
             <Flex>
                 <Emoji text={response.emotion} />
-                <Text opacity={0.5} fontSize={[1, 2, 3]}>{formatDate(response.created_at)}</Text>
+                <Text mx={[1]} opacity={0.5} fontSize={[1, 2, 3]}>{formatDate(response.created_at)}</Text>
                 <Text fontSize={[1, 2, 3]}> {response.text}</Text>
             </Flex>
             {response.contact_details &&
