95fee298951d607fb20ea53eb6b1950487cdb6aa
olaven <olav@sundfoer.com>
2020-08-20 12:06:24 +0000

    Now using loader component on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,40 +2,16 @@ import React from "react";
 import { UserContext } from "../context/UserContext";
 import { PageSection } from "../components/Home/PageSection/PageSection";
 import { IntroSection } from "../components/Home/IntroSection/IntroSection";
-import { GetServerSidePropsContext } from "next";
-import { get, OK } from "node-kall";
-
-/* export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-
-    const host = context.req.headers.host
-    console.log("host", host);
-    const response = await fetch(`http://localhost:3000/api/auth/me`);
-
-    const prefetchedAuthUser = response.status === 200 ?
-        await response.json() :
-        null
-
-    console.log("response", response);
-    return {
-        props: {
-            prefetchedAuthUser
-        }
-    }
-} */
+import { Loader } from "../components/tiny/loader";
 
 const IndexPage = () => {
 
     const { authUser, loading } = React.useContext(UserContext);
 
-    if (loading) return <div>test hei</div>
-    if (authUser) {
-
-        return <PageSection user={authUser} />
-    } else {
-
-        return <IntroSection />
-    }
-
+    if (loading) return <Loader size={150} />
+    return authUser ?
+        <PageSection user={authUser} /> :
+        <IntroSection />
 };
 
 export default IndexPage
