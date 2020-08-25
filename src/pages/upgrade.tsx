2dbb0357ec98617feb690da1f020881b74cecffd
olaven <olav@sundfoer.com>
2020-08-25 12:26:04 +0000

    first pass at cancel subscription-API. Working on GUI

--- src/pages/upgrade.tsx
+++ src/pages/upgrade.tsx
@@ -1,19 +1,18 @@
-import { Heading, Text, Flex, Box } from "rebass";
-import { useState } from "react";
+import { Heading, Text, Flex, Box, Button } from "rebass";
+import { useContext, useState } from "react";
 import { useProducts } from "../effects/useProducts";
 import { PaymentCard } from "../components/Upgrade/PaymentCard";
 import { ProductCard } from "../components/Upgrade/ProductCard";
 import * as text from "../text"
+import { UserContext } from "../context/UserContext";
+import { database } from "faker";
 
-
-
-const Upgrade = () => {
+const PriceAlternatives = () => {
 
     const [selectedPriceId, setSelectedPriceId] = useState("");
     const products = useProducts();
 
     return <>
-
         <Heading fontSize={[3, 4, 5]} textAlign="center">{text.upgrade.heading}</Heading>
         <Text fontSize={[1, 2, 3]} textAlign="center">{text.upgrade.inDevelopmentWarning}</Text>
         <Flex p={[2, 3, 4]}>
@@ -31,9 +30,41 @@ const Upgrade = () => {
             </Box>
             <Box width={[0, 1 / 3]} />
         </Flex>
-
-
     </>
+}
+
+const Downgrade = () => {
+
+    const { databaseUser, authUser } = useContext(UserContext);
+
+    const onCancel = async () => {
+
+
+    }
+
+    return <Box>
+        <Heading>Thank you, {authUser.name}</Heading>
+        Du har _dette_ abbonnemenetet
+        HVORFOR KRETS ER FINT OG HVORFOR DET ER SAA FLOTT AT DE BRUKER DET
+        MORSOM GIF?
+
+        Kanseller-knapp
+
+
+        <Button onClick={onCancel} backgroundColor="failure">
+            Kanseller
+        </Button>
+    </Box>
+}
+
+const Upgrade = () => {
+
+    const { databaseUser } = useContext(UserContext);
+
+    const isSubscriber = databaseUser?.free_premium || databaseUser?.subscription_id
+    return isSubscriber ?
+        <Downgrade /> :
+        <PriceAlternatives />
 }
 
 export default Upgrade; 
