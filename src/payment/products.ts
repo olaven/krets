e669f4cb8b7698120c41fc94b0aa44cf1f6ec684
olaven <olav@sundfoer.com>
2020-09-03 11:09:16 +0000

    product is not fetched by using subscription

--- src/payment/products.ts
+++ src/payment/products.ts
@@ -3,5 +3,10 @@ import { stripe } from "./stripe";
 export const getProducts = async () =>
     (await stripe.products.list()).data;
 
-export const getProduct = (id: string) =>
-    stripe.products.retrieve(id); 
+
+export const getProductBySubscription = async (subscriptionId: string) => {
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+    const productId = subscription.plan.product as string;
+    return stripe.products.retrieve(productId);
+}
