9254694c9c08a0279be434cbf9cea853d45b29c2
olaven <olav@sundfoer.com>
2020-10-29 15:07:19 +0000

    working on auth0 workaround. Pretty happy with current API

--- tests/integration/integration/pages/creation.test.ts
+++ tests/integration/integration/pages/creation.test.ts
@@ -1,15 +1,38 @@
 import * as uiText from "../../../../src/text";
+import { randomUser } from "../../../unit/database/databaseTestUtils";
+import { database } from "../../../../src/database/database";
 describe("Testing creation of pages", () => {
 
-    it.only("Is possible to create a page with the page creator", () =>
-        cy
-            .server()
-            .route2('GET', '/api/auth/me', { fixture: "authuser.json" })
-            .route2("GET", "/api/users/*", { fixture: "user.json" })
+    /**
+     * Wrapper for test functions that need a logged in user. 
+     * Mocking auth0 endpoints and creating a user in the database. 
+     */
+    const withUser = (id: string, runner: (cy: Cypress.cy) => any) =>
+        async () => {
+
+
+            //FIXME: somehow add user with same ID as auth0 mock 
+            /* await database.users.createUser(
+                randomUser(id)
+            ) */
+
+            runner(
+                cy
+                    .server()
+                    .route2("GET", "/api/auth/me", { fixture: "authuser.json" })
+            )
+        }
+
+    //FIXME: test not done yet, need to get `withUser` abstraction working  
+    it.only("is possible to create a page if logged in at the home page",
+        withUser("some-user", cy => cy
+
             .visit("/")
             .contains(uiText.pageCreator.placeholder)
-            .type("Some page name")
+            .type("Hello typing :D")
+        )
     );
+
 
     it("Can navigate to about page", () =>
         cy
