b37756f0777dd06ca83c3be7f4baaf6204d95e69
olaven <olav@sundfoer.com>
2020-10-29 17:33:21 +0100

    working on user task

--- tests/integration/integration/pages/creation.test.ts
+++ tests/integration/integration/pages/creation.test.ts
@@ -1,37 +1,49 @@
 import * as uiText from "../../../../src/text";
+
+//NOTE: should collect some of thes in non-unit files 
 import { randomUser } from "../../../unit/database/databaseTestUtils";
+import { randomAuthUser } from "../../../unit/api/apiTestUtils";
 import { database } from "../../../../src/database/database";
 describe("Testing creation of pages", () => {
+
 
     /**
      * Wrapper for test functions that need a logged in user. 
      * Mocking auth0 endpoints and creating a user in the database. 
      */
-    const withUser = (id: string, runner: (cy: Cypress.cy) => any) =>
-        async () => {
+    const withUser = (id: string) =>
+        (callback: (cy: Cypress.cy) => any) =>
+            () => {
 
+                callback(
+                    cy
+                        .task("createUser", id)
+                        .server()
+                        .route2("GET", "/api/auth/me", { fixture: "randomAuthUser()" })
+                )
+            }
 
-            //FIXME: somehow add user with same ID as auth0 mock 
-            /* await database.users.createUser(
-                randomUser(id)
-            ) */
-
-            runner(
-                cy
-                    .server()
-                    .route2("GET", "/api/auth/me", { fixture: "authuser.json" })
-            )
-        }
 
     //FIXME: test not done yet, need to get `withUser` abstraction working  
-    it.only("is possible to create a page if logged in at the home page",
-        withUser("some-user", cy => cy
 
-            .visit("/")
-            .contains(uiText.pageCreator.placeholder)
-            .type("Hello typing :D")
+    it.only("thing", () => cy
+        .task("createUser")
+        .route2("GET", "/api/auth/me", { fixture: "randomAuthUser()" })
+        .visit("/")
+        .contains(uiText.pageCreator.placeholder)
+        .type("Hello from currying variant")
+    )
+
+    it(
+        "Currying variant of mock",
+        withUser("some-user")(cy =>
+            cy
+                .visit("/")
+                .contains(uiText.pageCreator.placeholder)
+                .type("Hello from currying variant")
         )
     );
+
 
 
     it("Can navigate to about page", () =>
