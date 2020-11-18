3d9667c084629991a347460993180b27dfc73c3d
olaven <olav@sundfoer.com>
2020-11-18 17:35:29 +0100

    WIP template conversion

--- src/pages/api/cron/email-summary-trigger.ts
+++ src/pages/api/cron/email-summary-trigger.ts
@@ -2,8 +2,9 @@ import { FORBIDDEN, ACCEPTED } from "node-kall";
 import { asyncForEach } from "../../../asyncForEach";
 import { database } from "../../../database/database";
 import { date } from "../../../date";
+import { emojidata } from "../../../emojidata";
 import { withErrorHandling, withMethodHandlers } from "../../../middleware/middleware";
-import { PageModel, UserModel, } from "../../../models/models";
+import { PageModel, ResponseModel, UserModel, } from "../../../models/models";
 
 const lastSevenDays = (page: PageModel) =>
     database.responses.getAfter(
@@ -33,7 +34,6 @@ const getSummaryData = async () => {
 }
 
 type PostMarkSummaryTemplate = {
-    greeting_name: string,
     pages: {
 
         name: string,
@@ -54,8 +54,41 @@ type PostMarkSummaryTemplate = {
 };
 
 
+const getTemplateQuestions = (response: ResponseModel) => {
 
-const toTemplate = ({ user, responses, answers }) => { }
+    //TOOD: IMPlement
+}
+
+
+const getTemplateResponses = (page: PageModel) => {
+
+    const responses = await lastSevenDays(page);
+    return responses.map(response => ({
+        emoji: emojidata[response.emotion],
+        contact_details: response.contact_details,
+        questions: getTemplateQuestions(response)
+    }));
+}
+
+const toTemplate = (pages: PageModel[]): PostMarkSummaryTemplate => {
+
+    const templatePages = pages.map(page => ({
+        name: page.name,
+        total_response_count_in_period: pages.length,
+        positive: true, // TODO: calculate 
+        percentage: 1, //TODO: calculate 
+        responses: getTemplateResponse(page)
+    }));
+
+    return {
+        pages: [
+            {
+                name: "some name",
+
+            }
+        ]
+    }
+}
 
 const triggerEmailSummary = async () => {
 
