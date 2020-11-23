fe71e35b2f02255ae717c96add0f0e65a7826bbf
olaven <olav@sundfoer.com>
2020-11-23 13:31:56 +0100

    Hello, PDF! - using pdfJS

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,19 +1,21 @@
 import { AdminWrapper } from "../components/AdminWrapper";
+import { jsPDF} from "jspdf";
+
 
 export default AdminWrapper(() => {
 
 
-    const onClick = async () => {
-
-        const repsonse = fetch("http://localhost:3000/api/cron/email-summary-trigger", {
-            headers: {
-                "x-krets-email-summary-secret": "some-random-token"
-            }
-        });
-        console.log(repsonse);
+    const onClick = () => {
+        
+        const pdf = new jsPDF(); 
+        pdf.setFontSize(55);
+        pdf.text("10, 10", 10, 10); 
+        pdf.setFontSize(15);
+        pdf.text("20, 20", 20, 20); 
+        pdf.save("test.pdf");
     }
 
     return <>
-        <button onClick={onClick}>Trigger Summary</button>
+        <button onClick={onClick}>Last ned PDF</button>
     </>
 })
