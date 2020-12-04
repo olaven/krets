14648a4c593a75f1297fbcf90061fec60ee6d369
olaven <olav@sundfoer.com>
2020-12-04 18:54:44 +0100

    layout is without rebass

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,28 +1,28 @@
 import { UserContext } from "../context/UserContext";
 import { useContext } from "react";
 import { useRouter } from "next/router";
-import { Box, Button, Flex, Link, Text, Image } from "rebass";
 import { AboutButton, GuidePageButton, LoginButton, LogoutButton, MyPageButton } from "./standard/buttons";
 import { styled } from "../stiches.config";
+import { RowContainer } from "./standard/Containers";
+
 
 const HeaderLogo = () => {
 
+    const LogoLink = styled("a", {
+        marginRight: "auto",
+
+        mobile: {
+            display: "none"
+        }
+    });
 
     const router = useRouter();
 
     return ["/[pageId]/embed"].includes(router.pathname) ?
         null :
-        <Flex color='primary' my={[1, 1, 2]} width={[1]}>
-            <Link href="/">
-                <Image
-                    src={"/logo.svg"}
-                    sx={{
-                        width: ['7.5%'],
-                    }}
-                />
-            </Link >
-            {/* <Text p={[0, 1, 2]} fontSize={7}>Krets.</Text> */}
-        </Flex>
+        <LogoLink href="/">
+            <img src={"/logo.svg"} height="100px"/>
+        </LogoLink >
 }
 /**
  * Shows the CornerButtons unless on 
@@ -53,48 +53,27 @@ const CornerButtons = () => {
         </>
 }
 
-const Footer = styled("div", {
-    position: "absolute", 
-    fontSize: "1em",
-    bottom: "-1em",
-    opacity: 0.5
-});
-
-
 const FullScreen = styled("div", {
-    backgroundColor: "orange",
     position: "absolute", 
     width: "100vw", 
     height: "100vh", 
+    backgroundColor: "$secondary",
     top: 0, 
     left: 0,
     minHeight: "100%"
 });
 
-export const Layout = (props) => {
+const Header = styled(RowContainer, {
+    marginTop: "$5", 
+    marginLeft: "$5",
+    justifyContent: "flex-end",
+}); 
 
-    return <>
+export const Layout = (props) => 
     <FullScreen>
-            <Box
-                minWidth={"100vw"}
-                minHeight={"100vh"}
-                sx={{
-                    fontFamily: "body"
-                }}
-                backgroundColor={"secondary"}
-            >
-                <Flex px={2}
-                    color='primary'
-                    alignItems='center'>
-
-                    <HeaderLogo />
-                    <CornerButtons />
-                </Flex>
-                {props.children}
-            </Box>
-        </FullScreen>
-        <Footer>
-            {/* NO FOOTER CONTENT */}
-        </Footer>
- </>
-};
+        <Header>
+            <HeaderLogo />
+            <CornerButtons />
+        </Header>
+        {props.children}
+    </FullScreen>
